acd6d251745b54dd2d0b3e00a49f7d6b4b67e7af
bahaeddine krifa <bahaeddinekrifa@gmail.com>
2026-05-14 22:02:13 +0100

    change script bat with chemin complet

--- scripts/init-topics.bat
+++ scripts/init-topics.bat
@@ -1,6 +1,17 @@
 @echo off
 echo 🔄 Initialisation des topics Kafka...
-docker exec smarthealth-kafka kafka-topics.sh --create --topic patient.created --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1 --if-not-exists
-docker exec smarthealth-kafka kafka-topics.sh --create --topic appointment.confirmed --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1 --if-not-exists
-echo ✅ Topics prêts.
+
+set KAFKA_CONTAINER=smarthealth-kafka
+set KAFKA_BIN=/opt/kafka/bin/kafka-topics.sh
+set BOOTSTRAP=localhost:9092
+
+echo ⏳ Création du topic 'patient.created'...
+docker exec %KAFKA_CONTAINER% %KAFKA_BIN% --create --topic patient.created --bootstrap-server %BOOTSTRAP% --partitions 1 --replication-factor 1 --if-not-exists
+
+echo ⏳ Création du topic 'appointment.confirmed'...
+docker exec %KAFKA_CONTAINER% %KAFKA_BIN% --create --topic appointment.confirmed --bootstrap-server %BOOTSTRAP% --partitions 1 --replication-factor 1 --if-not-exists
+
+echo ✅ Topics créés avec succès.
+echo 📋 Liste des topics actifs :
+docker exec %KAFKA_CONTAINER% %KAFKA_BIN% --list --bootstrap-server %BOOTSTRAP%
 pause
